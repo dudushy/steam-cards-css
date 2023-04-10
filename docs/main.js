261b6d5801f271c9ea48a41bcd7dddfa0f5a3368
dudushy <eduardotalarico1337@gmail.com>
2023-04-09 22:10:18 -0300

    Something is wrong

---
+++ docs/main.js
@@ -0,0 +1,28 @@
+const maxX = 15;
+const maxY = 15;
+
+const card = document.getElementById('card');
+
+card.addEventListener('mousemove', (e) => {
+    const x = e.offsetX;
+    const y = e.offsetY;
+    // console.log('x:', x, 'y:', y);
+
+    const cardRect = card.getBoundingClientRect();
+    // console.log('cardRect:', cardRect);
+    
+    const halfHeight = cardRect.height / 2;
+    const halfWidth = cardRect.width / 2;    
+
+    const rotationX = ((y - halfHeight) / halfHeight) * maxY;
+    const rotationY = ((x - halfWidth) / halfWidth) * maxX;
+    console.log('x:', x, 'y:', y, 'rotationX:', rotationX, 'rotationY:', rotationY);
+
+    card.style.transition = `none`;
+    card.style.transform = `rotateY(${rotationY}deg) rotateX(${rotationX}deg)`;
+});
+
+card.addEventListener('mouseleave', () => {
+    card.style.transition = `transform 0.5s ease-in-out`;
+    card.style.transform = `rotate(0)`;
+});
